6157594e0603a2b812ace007fbb89de95982ecf8
Markus Must <markusmust@users.noreply.github.com>
2019-06-21 09:39:41 +0300

    kiire uuendus

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ function show(elementID) {
         alert("no such element");
         return;
     }
-    
+
     var pages = document.getElementsByClassName('page');
     for(var i = 0; i < pages.length; i++) {
         pages[i].style.display = 'none';
     }
     ele.style.display = 'block';
+    if (ele.id == "Page1") {
+        ele.style.display = 'flex';
+    }
 }
 
 
